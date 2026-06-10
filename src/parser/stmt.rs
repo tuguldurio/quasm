0f966b3eb8ba484ef1f72ec5db52d7fa07a614ff
perffeith <on.tuguldur@gmail.com>
2026-06-10 14:34:28 -0400

    split parser code into more separate files

---
+++ src/parser/stmt.rs
@@ -0,0 +1,100 @@
+use crate::lexer::TokenKind;
+use super::ast::*;
+use super::Parser;
+use super::ParseError;
+
+impl Parser {
+    pub fn parse_program(&mut self) -> Result<Program, ParseError> {
+        let mut statements = Vec::new();
+        self.skip_newlines();
+
+        while !self.peek_is(TokenKind::Eof) {
+            statements.push(self.parse_statement()?);
+            self.skip_newlines();
+        }
+
+        Ok(Program { statements })
+    }
+
+    fn parse_statement(&mut self) -> Result<Stmt, ParseError> {
+        match self.peek() {
+            TokenKind::Func => Ok(Stmt::Func(self.parse_func_decl()?)),
+            TokenKind::Let => Ok(Stmt::Let(self.parse_let_statement()?)),
+            TokenKind::Type => Ok(Stmt::Type(self.parse_type_decl()?)),
+            TokenKind::Struct => Ok(Stmt::Struct(self.parse_struct_decl()?)),
+            _ => Ok(Stmt::Expr(self.parse_expr()?)),
+        }
+    }
+
+    fn parse_func_decl(&mut self) -> Result<FuncStmt, ParseError> {
+        self.consume(TokenKind::Func)?;
+        let name = self.parse_identifier()?;
+        let params = self.parse_func_params()?;
+        let ret = if self.peek_is(TokenKind::Arrow) {
+            self.advance();
+            Some(self.parse_type()?)
+        } else {
+            None
+        };
+        let body = self.parse_block()?;
+        Ok(FuncStmt { name, params, ret, body })
+    }
+
+    fn parse_func_params(&mut self) -> Result<Vec<Param>, ParseError> {
+        self.parse_comma_list(TokenKind::LParen, TokenKind::RParen, "parameter", |p| {
+            let name = p.parse_identifier()?;
+            let ty = p.parse_type_annotation()?
+                .ok_or_else(|| p.err("expected type annotation for parameter"))?;
+            Ok(Param { name, ty })
+        })
+    }
+
+    fn parse_let_statement(&mut self) -> Result<LetStmt, ParseError> {
+        self.consume(TokenKind::Let)?;
+        let name = self.parse_identifier()?;
+        let ty = self.parse_type_annotation()?;
+        self.consume(TokenKind::Eq)?;
+        let value = self.parse_expr()?;
+        Ok(LetStmt { name, ty, value })
+    }
+
+    fn parse_type_decl(&mut self) -> Result<TypeStmt, ParseError> {
+        self.consume(TokenKind::Type)?;
+        let name = self.parse_identifier()?;
+        let ty_params = self.parse_ty_params()?;
+        let variants = self.parse_braced_list("variant", |p| p.parse_type_variant())?;
+        Ok(TypeStmt { name, ty_params, variants })
+    }
+
+    fn parse_type_variant(&mut self) -> Result<TypeVariant, ParseError> {
+        let name = self.parse_identifier()?;
+
+        let ty_fields = if self.peek_is(TokenKind::LParen) {
+            self.parse_comma_list(TokenKind::LParen, TokenKind::RParen, "type field", |p| p.parse_type())?
+        } else {
+            Vec::new()
+        };
+
+        Ok(TypeVariant { name, ty_fields })
+    }
+
+    fn parse_struct_decl(&mut self) -> Result<StructStmt, ParseError> {
+        self.consume(TokenKind::Struct)?;
+        let name = self.parse_identifier()?;
+        let ty_params = self.parse_ty_params()?;
+        let fields = self.parse_braced_list("field", |p| p.parse_struct_field())?;
+        Ok(StructStmt { name, ty_params, fields })
+    }
+
+    fn parse_struct_field(&mut self) -> Result<StructField, ParseError> {
+        let name = self.parse_identifier()?;
+        self.consume(TokenKind::Colon)?;
+        let ty = self.parse_type()?;
+        Ok(StructField { name, ty })
+    }
+
+    pub(super) fn parse_block(&mut self) -> Result<Block, ParseError> {
+        let stmts = self.parse_braced_list("statement", |p| p.parse_statement())?;
+        Ok(Block { stmts })
+    }
+}
---
+++ src/parser/types.rs
@@ -0,0 +1,62 @@
+use crate::lexer::TokenKind;
+use super::ast::*;
+use super::Parser;
+use super::ParseError;
+
+impl Parser {
+    pub(super) fn parse_type_annotation(&mut self) -> Result<Option<Type>, ParseError> {
+        if !self.peek_is(TokenKind::Colon) {
+            return Ok(None);
+        }
+        self.advance();
+        Ok(Some(self.parse_type()?))
+    }
+
+    // type parameter list of type/struct declaration
+    pub(super) fn parse_ty_params(&mut self) -> Result<Vec<Identifier>, ParseError> {
+        if !self.peek_is(TokenKind::LParen) {
+            return Ok(Vec::new());
+        }
+        let params = self.parse_comma_list(TokenKind::LParen, TokenKind::RParen, "type parameter", |p| p.parse_identifier())?;
+        if params.is_empty() {
+            return Err(self.err("type parameter list cannot be empty"));
+        }
+        Ok(params)
+    }
+
+    pub(super) fn parse_type(&mut self) -> Result<Type, ParseError> {
+        match self.peek() {
+            TokenKind::LBracket => {
+                self.advance();
+                let inner = self.parse_type()?;
+                self.consume(TokenKind::RBracket)?;
+                Ok(Type::Array(Box::new(inner)))
+            }
+            TokenKind::Func => {
+                self.advance();
+                let params = self.parse_comma_list(TokenKind::LParen, TokenKind::RParen, "parameter type", |p| p.parse_type())?;
+                let ret = if self.peek_is(TokenKind::Arrow) {
+                    self.advance();
+                    Some(Box::new(self.parse_type()?))
+                } else {
+                    None
+                };
+                Ok(Type::Func { params, ret })
+            }
+            TokenKind::Identifier(_) => {
+                let name = self.parse_identifier()?;
+                let args = if self.peek_is(TokenKind::LParen) {
+                    let args = self.parse_comma_list(TokenKind::LParen, TokenKind::RParen, "type argument", |p| p.parse_type())?;
+                    if args.is_empty() {
+                        return Err(self.err("type argument list cannot be empty"));
+                    }
+                    args
+                } else {
+                    Vec::new()
+                };
+                Ok(Type::Named { name, args })
+            }
+            other => Err(self.err(format!("expected type, got {:?}", other)))
+        }
+    }
+}
