d54faca0353e67f7e805a26990433603e3f00a4b
tuguldurio <on.tuguldur@gmail.com>
2026-07-16 22:36:41 -0400

    idea: cache wasm level func signatures at codegen

--- src/codegen/mod.rs
+++ src/codegen/mod.rs
@@ -1,3 +1,23 @@
+#![allow(unused)]
+use std::collections::HashMap;
+
+use wasm_encoder::{
+    TypeSection,
+    ValType
+};
+
 pub struct Codegen {
-    
+    types: TypeSection,
+    func_sig_cache: HashMap<(Vec<ValType>, Vec<ValType>), u32>
+}
+
+impl Codegen {
+    fn func_sig_index(&mut self, params: Vec<ValType>, results: Vec<ValType>) -> u32 {
+        let key = (params.clone(), results.clone());
+        *self.func_sig_cache.entry(key).or_insert_with(|| {
+            let idx = self.types.len();
+            self.types.ty().function(params, results);
+            idx
+        })
+    }
 }
